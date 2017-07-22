402b2e7a734c4508c4cca2e65811241626d6f369
bearski <santosh.sewlal@gmail.com>
2017-07-22 14:11:58 +0200

    Creating the Query Controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services. Note the use of ngRoute.
-var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
+var app = angular.module('meanMapApp', ['addCtrl', 'queryCtrl', 'geolocation', 'gservice', 'ngRoute'])
 
   // Configures Angular routing -- showing the relevant view and controller when needed.
   .config(function($routeProvider) {
@@ -9,6 +9,7 @@ var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice', 'n
       templateUrl: 'partials/addForm.html',
       // Find Teammates Control Panel
     }).when('/find', {
+      controller: 'queryCtrl',
       templateUrl: 'partials/queryForm.html',
       // All else forward to the Join Team Control Panel
     }).otherwise({
